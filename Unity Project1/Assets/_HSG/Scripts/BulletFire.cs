e9bb8e27bc47d5474f5a7679296f643439e1a342
홍승기 <tmd2090@naver.com>
2020-11-11 11:05:06 +0900

    플레이어 이동, 보조 비행체 생성, 총알 발사

---
+++ Unity Project1/Assets/_HSG/Scripts/BulletFire.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletFire : MonoBehaviour
+{
+    public GameObject bulletFactory;    //총알 공장(프리팹)
+    public Transform firePoint;         //총알 발사위치
+    float time;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Fire();
+    }
+
+    private void Fire()
+    {
+        time += Time.deltaTime;
+
+        if(time>0.5)
+        {
+            GameObject bullet = Instantiate(bulletFactory);
+            bullet.transform.position = firePoint.position;
+            time = 0;
+        }
+    }
+
+}
---
+++ Unity Project1/Assets/_HSG/Scripts/SetActive.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetActive : MonoBehaviour
+{
+    public GameObject cube;
+    private bool state;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if(Input.GetKeyDown(KeyCode.Space))
+       {
+            state = !state;
+            cube.SetActive(state);
+       }
+    }
+}
